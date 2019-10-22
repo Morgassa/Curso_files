583e8675e486f3e995bbb4b9c484401d5a6c534e
Morgassa <amadeudeassuncao@gmail.com>
2019-10-22 18:12:38 -0300

    First Step!

--- Desafio 02.py
+++ Desafio 02.py
@@ -1,2 +1,13 @@
 # 32. Desafio Operadores Lógicos
 
+# Os trabalhos
+
+trabalho_terça = True
+trabalho_quinta = False
+
+tv50 = trabalho_terça and trabalho_quinta
+sorvete = trabalho_terça or trabalho_quinta
+tv32 = trabalho_terça != trabalho_quinta
+mais_saude = not sorvete
+
+print("Tv50={} Tv32={} sorvete={} saúde={}".format(tv50, tv32, sorvete, mais_saude))
